615ecff63350973d8bd42e16102cec6850504a11
Wer <davigwerneck@gmail.com>
2022-10-21 11:47:21 -0300

    another annoying cmmets removal and player spd

--- PyGProj/Project/code/main.py
+++ PyGProj/Project/code/main.py
@@ -5,11 +5,9 @@ from settings import *
 from level import Level 
 from player import *
 from camera import CameraGroup
-class Game:
-    '''Classe que vai ser responsável por rodar o jogo, e por gerenciar o menu, o level, e tudo mais. A classe principal do game. '''   
+class Game: 
     
     def __init__(self):
-        '''Construtor do game, aqui vai ser inicializado tudo que for necessário para o game rodar. '''
         # general setup
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGTH))
@@ -19,6 +17,7 @@ class Game:
 
         self.level = Level()  # instancia o level
         self.camera_group = CameraGroup() # instancia o grupo de camera
+        
         # sound
         
         '''main_sound = pg.mixer.Sound('../audio/main.ogg') # carrega o som principal e o coloca em loop
@@ -26,7 +25,6 @@ class Game:
         main_sound.play(loops=-1)
         '''
     def run(self):
-        '''Este método vai executar o game em si, e ao mesmo tempo vai dar update nele. Um loop infinito que vai carregar constantemente o level, o player e outras entidades. '''
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
