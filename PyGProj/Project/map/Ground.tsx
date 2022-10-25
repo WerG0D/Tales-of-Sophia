7460e78a8ed5437eaf9cb54a7109dea552655844
Wer <davigwerneck@gmail.com>
2022-10-25 10:05:02 -0300

    testing tiled

---
+++ PyGProj/Project/map/Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Ground" tilewidth="64" tileheight="64" tilecount="50" columns="10">
+ <image source="../graphs/tilemap/Dark_lvl0.png" width="640" height="320"/>
+</tileset>
---
+++ PyGProj/Project/map/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="objects" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="../graphs/objects/0.png" width="128" height="128"/>
+</tileset>
