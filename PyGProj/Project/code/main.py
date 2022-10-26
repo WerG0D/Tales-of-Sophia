51b7d4752249dfc427811f1332902f84e4f4dfb4
Wer <davigwerneck@gmail.com>
2022-10-26 08:37:35 -0300

    peped main.py

--- PyGProj/Project/code/main.py
+++ PyGProj/Project/code/main.py
@@ -1,4 +1,3 @@
-
 import pygame as pg
 import sys
 from settings import WIDTH, HEIGHT, FPS, WATER_COLOR, CONTROLKEYS
@@ -11,16 +10,24 @@ class Game:
         
         # general setup
         pg.init()
+        
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        
         pg.display.set_caption('Tales of sophia')
+        
         self.clock = pg.time.Clock()
+        
         pg.event.set_grab(True)
+        
         self.level = Level()  # instancia o level
 
         # sound
         main_sound = pg.mixer.Sound('../audio/main.ogg') # carrega o som principal e o coloca em loop
+        
         main_sound.set_volume(0.5)
-        main_sound.play(loops=-1)        
+        
+        main_sound.play(loops=-1)
+                
     def run(self):
         
         while True:
@@ -28,14 +35,20 @@ class Game:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+                
                 if event.type == pg.KEYDOWN and event.key == CONTROLKEYS['exit']:
                         pg.quit()
                         sys.exit()
                 game.level.visible_sprites.zoom_scroll(event) ## cara, essa parte do codigo busca a instasncia da camera e executa a funcao de zoom no scroll do mouse
+            
             self.screen.fill(WATER_COLOR)
+            
             self.level.run()
+            
             pg.display.update()
+            
             self.clock.tick(FPS)
+            
             keys = pg.key.get_pressed()
             if keys[pg.K_o]:
                 print(self.level.player.rect.x, self.level.player.rect.y) #printa a posição do player
@@ -54,7 +67,10 @@ class Menu():
         game.run()
     
     menugame = pg_menu.Menu('Tales of Sophia', WIDTH, HEIGHT, theme=pg_menu.themes.THEME_DARK)
+    
     menugame.add.button('Start Game', start_game)
+    
     menugame.add.button('Quit', pg_menu.events.EXIT)
+    
     menugame.mainloop(surface)
 #
