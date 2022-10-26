fadcffc7c27aef3dfc9326c64817a765db360dc5
Wer <davigwerneck@gmail.com>
2022-10-26 07:58:28 -0300

    removing YAGNIS

--- PyGProj/Project/code/level.py
+++ PyGProj/Project/code/level.py
@@ -1,6 +1,6 @@
 from cgi import test
 import pygame as pg
-from settings import WORLD_MAP , TILESIZE
+from settings import TILESIZE
 from tile import Tile
 from player import Player
 from camera import CameraGroup
@@ -8,7 +8,6 @@ from debug import debug
 from support import import_csv , import_folder
 
 class Level:
-    '''Essa classe basicamente é o coração do jogo. O level é uma espécie de conteiner que vai conter todos os objetos/sprites do jogo, Sejam eles visiveis ou não. O funcionamento divide os sprites em duas classes, sprites visiveis e sprites de obstáculo, cada um com suas propriedades. Um obstáculo pode ter os dois grupos ao mesmo tempo, como por exemplo uma árvore com hitbox, que vai ter colisão e vai ser desenhada na tela. '''
 
     def __init__(self):
         
@@ -20,7 +19,7 @@ class Level:
         self.obstacle_sprites = pg.sprite.Group() # Grupo de sprites que são obstáculos, ou seja, não podem ser atravessados e terão colisão com o player. Tudo que tiver essa proprieadade vai ter esse subgrupo.
 
 
-        self.display_surface = pg.display.get_surface() # Getter do tamanho da tela: (é mais prático criar isto aqui para deixar a possibilidade de mudar o tamanho da tela no futuro pelo menu do game.)
+        self.display_surface = pg.display.get_surface() 
 
         
         #Sprite Setup:
@@ -28,8 +27,7 @@ class Level:
         
 
     def create_map(self):
-        '''Este método vai criar o mapa do jogo. Basicamente ele vai criar um grid de tiles, e vai colocar os tiles no lugar certo. A ideia é que o mapa seja criado a partir de um arquivo de texto, que vai ser lido e vai criar o mapa. O mapa nada mais é que uma matriz de tiles, onde cada tile vai ter uma posição e um tipo. A posição dos tiles vai ser definida pelo arquivo de texto, e o tipo vai ser definido pelo arquivo de texto também. Cada posição vai variar de acordo com o tamanho do Tile (no nosso caso, 64). Em suma, no local 0,0 vai ter um tile, no local 64,0 vai ter outro tile, e assim por diante. '''
-        #criando um dicionario dos layers do mapa? eu acho, gabriel do futuro aqui, eu acho nao, eu tenho certeza, essa funcao basicamnete importa o arquivo csv e retorna uma matriz com os valores do arquivo pra noz usar como mapa
+        
         layouts = {
             'invis': import_csv('../map/testmapwow_terrain.csv'),
             'terrain': import_csv('../map/testmapwow_terrain.csv'),
@@ -54,23 +52,14 @@ class Level:
                         if style == 'objects':
                             # surf = graphics['objects'][int(col)]
                             surf = graphics['objects'][0]
-                            print("ATENCAO GABRIEL DO FUTURO, AQUI VAI SER CRIADO O SPRITE DO OBJETO, AGORA VOCE TA USANDO SO UM 0 ALI EM CIMA ENTrE CAIXAS PQ SO TE 1 UNICO SPRITE, SE ADICIONAR OUTROs vai ferrar o codigo, FAVOR MEXER AQUI NO FUTURO")
+                            '''ATENCAO GABRIEL DO FUTURO, AQUI VAI SER CRIADO O SPRITE DO OBJETO, AGORA VOCE TA USANDO SO UM 0 ALI EM CIMA ENTrE CAIXAS PQ SO TE 1 UNICO SPRITE, SE ADICIONAR OUTROs vai ferrar o codigo, FAVOR MEXER AQUI NO FUTURO'''
                             Tile((x, y), [self.visible_sprites,self.obstacle_sprites], 'object', surf)
 
                         if style == 'player':
                             self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
-         # Cria o player no mapa, 500 500 e a posicao x e y dele, basico
 
     def run(self):
-        '''Este método vai executar o level em si, e ao mesmo tempo vai dar update nele. Basicamente vai desenhar as coisas na tela. '''
+        
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.hitbox)
-
-
-        
-        
-        #         if col  == 'x':
-        #             Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
-        #         if col == 'p':
-        #             self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
