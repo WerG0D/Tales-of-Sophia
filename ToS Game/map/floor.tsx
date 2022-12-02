a8ce48d4ddc916d1a05467fc4e54f4df27f96491
Wer <davigwerneck@gmail.com>
2022-12-02 07:26:22 -0300

    i tried.... i have fail.

--- ToS Game/map/floor.tsx
+++ ToS Game/map/floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="floor" tilewidth="64" tileheight="64" tilecount="550" columns="22">
- <image source="C:/Users/gabri/Documents/GitHub/PyGProject/PyGProj/Project/graphs/tilemap/Floor.png" width="1408" height="1600"/>
+ <image source="../graphs/tilemap/Floor.png" width="1408" height="1600"/>
 </tileset>
--- ToS Game/map/grass.tsx
+++ ToS Game/map/grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="grass" tilewidth="64" tileheight="64" tilecount="4" columns="2">
- <image source="C:/Users/gabri/Documents/GitHub/PyGProject/PyGProj/Project/graphs/grass/grass_detail_full.png" width="128" height="128"/>
+ <image source="../graphs/grass/grass_detail_full.png" width="128" height="128"/>
 </tileset>
